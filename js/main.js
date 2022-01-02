f92676588687600c87e1dabd94b4e1c2773851c7
ISLOMIDDIN98 <najmiddinovislomiddin2gmail.com>
2022-01-02 20:26:51 +0900

    add ...

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 
 let container = document.querySelector(".menu-btn");
 
+ Nav = document.querySelector(".menu-nav");
+
 container.addEventListener("click", function(e){
 container.classList.toggle("menu-btn-active");
+Nav.classList.toggle('menu-nav-active');
+
 });
